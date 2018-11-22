32e8991faaeef4a1e2c63324649c4a59a14397e5
Andreas Sandskär <ekrarna@gmail.com>
2018-11-22 10:51:07 +0100

    removed extra files and used ng-if to clear out news

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// open var
+
+
 var app = angular.module("myApp", []);
 app.controller("myCtr", function ($scope, $http) {
     $http.get("https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=c84efbb45d9a46b29d3404a6e0953d17")
     .then(function(myData){
     $scope.news=myData.data.articles;
+
     });
 });
